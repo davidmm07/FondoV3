e79eacc78159589d8525ccd9a3462aca1c3ec8d9
Cristian <Cristian@Cristian-PC>
2016-03-25 12:07:14 -0500

    Creación de clases

--- src/java/datos/SocioDAO.java
+++ src/java/datos/SocioDAO.java
@@ -5,10 +5,53 @@
  */
 package datos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import negocio.Socio;
+import util.RHException;
+import util.ServiceLocator;
+
 /**
  *
  * @author David Morales
  */
 public class SocioDAO {
-    
+
+    public SocioDAO() {
+
+    }
+
+    public void agregarSocio(Socio socio) throws SQLException, RHException {
+        try {
+            String strSQL = "INSERT INTO SOCIO(K_IDSOCIO,N_NOMSOCIO,N_APESOCIO,O_OCUPACION,N_TARJPROFESIONAL,O_ECIVIL,I_SEXO,"
+                    + "O_DIR_DOMIC,O_DIR_TRAB,O_EMAIL,O_TEL_DOMIC,O_TEL_TRAB,F_INGRESO,N_ESTADO_CK) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,"
+                    + "TO_DATE(SYSDATE,'DD/MM/YY'),'PENDIENTE')";
+            Connection conexion = ServiceLocator.getInstance().tomarConexion();
+            PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
+            prepStmt.setInt(1, socio.getK_idSocio());
+            prepStmt.setString(2, socio.getN_nomSocio());
+            prepStmt.setString(3, socio.getN_apeSocio());
+            prepStmt.setString(4, socio.getO_ocupacion());
+            prepStmt.setString(5, socio.getN_tarjProfesional());
+            prepStmt.setString(6, socio.getO_eCivil());
+            prepStmt.setString(7, socio.getI_sexo());
+            prepStmt.setString(8, socio.getO_dir_domic());
+            prepStmt.setString(9, socio.getO_dir_trab());
+            prepStmt.setString(10, socio.getO_email());
+            prepStmt.setString(11, socio.getO_tel_domic());
+            prepStmt.setString(12, socio.getO_tel_domic());
+            prepStmt.setString(13, socio.getO_tel_trab());
+            prepStmt.executeUpdate();
+            prepStmt.close();
+            ServiceLocator.getInstance().commit();
+
+        } catch (SQLException e) {
+            throw new RHException("EmpleadoDAO", "No pudo crear el empleado" + e.getMessage());
+        } finally {
+            ServiceLocator.getInstance().liberarConexion();
+        }
+
+    }
+
 }
--- src/java/negocio/Socio.java
+++ src/java/negocio/Socio.java
@@ -11,6 +11,155 @@ package negocio;
  */
 public class Socio {
     
+    private int  k_idSocio;
+    private String n_nomSocio;
+    private String n_apeSocio;
+    private String o_ocupacion;
+    private String n_tarjProfesional;
+    private String o_eCivil;
+    private String i_sexo;
+    private String o_dir_domic;
+    private String o_dir_trab;
+    private String o_email;
+    private String o_tel_domic;
+    private String o_tel_trab;
+    private String f_ingreso;
+    private String f_retiro;
+    private String o_caus_retiro;
+    private String n_estado_ck;
+    
+    public Socio(){
+        
+    }
+
+    public int getK_idSocio() {
+        return k_idSocio;
+    }
+
+    public void setK_idSocio(int k_idSocio) {
+        this.k_idSocio = k_idSocio;
+    }
+
+    public String getN_nomSocio() {
+        return n_nomSocio;
+    }
+
+    public void setN_nomSocio(String n_nomSocio) {
+        this.n_nomSocio = n_nomSocio;
+    }
+
+    public String getN_apeSocio() {
+        return n_apeSocio;
+    }
+
+    public void setN_apeSocio(String n_apeSocio) {
+        this.n_apeSocio = n_apeSocio;
+    }
+
+    public String getO_ocupacion() {
+        return o_ocupacion;
+    }
+
+    public void setO_ocupacion(String o_ocupacion) {
+        this.o_ocupacion = o_ocupacion;
+    }
+
+    public String getN_tarjProfesional() {
+        return n_tarjProfesional;
+    }
+
+    public void setN_tarjProfesional(String n_tarjProfesional) {
+        this.n_tarjProfesional = n_tarjProfesional;
+    }
+
+    public String getO_eCivil() {
+        return o_eCivil;
+    }
+
+    public void setO_eCivil(String o_eCivil) {
+        this.o_eCivil = o_eCivil;
+    }
+
+    public String getI_sexo() {
+        return i_sexo;
+    }
+
+    public void setI_sexo(String i_sexo) {
+        this.i_sexo = i_sexo;
+    }
+
+    public String getO_dir_domic() {
+        return o_dir_domic;
+    }
+
+    public void setO_dir_domic(String o_dir_domic) {
+        this.o_dir_domic = o_dir_domic;
+    }
+
+    public String getO_dir_trab() {
+        return o_dir_trab;
+    }
+
+    public void setO_dir_trab(String o_dir_trab) {
+        this.o_dir_trab = o_dir_trab;
+    }
+
+    public String getO_email() {
+        return o_email;
+    }
+
+    public void setO_email(String o_email) {
+        this.o_email = o_email;
+    }
+
+    public String getO_tel_domic() {
+        return o_tel_domic;
+    }
+
+    public void setO_tel_domic(String o_tel_domic) {
+        this.o_tel_domic = o_tel_domic;
+    }
+
+    public String getO_tel_trab() {
+        return o_tel_trab;
+    }
+
+    public void setO_tel_trab(String o_tel_trab) {
+        this.o_tel_trab = o_tel_trab;
+    }
+
+    public String getF_ingreso() {
+        return f_ingreso;
+    }
+
+    public void setF_ingreso(String f_ingreso) {
+        this.f_ingreso = f_ingreso;
+    }
+
+    public String getF_retiro() {
+        return f_retiro;
+    }
+
+    public void setF_retiro(String f_retiro) {
+        this.f_retiro = f_retiro;
+    }
+
+    public String getO_caus_retiro() {
+        return o_caus_retiro;
+    }
+
+    public void setO_caus_retiro(String o_caus_retiro) {
+        this.o_caus_retiro = o_caus_retiro;
+    }
+
+    public String getN_estado_ck() {
+        return n_estado_ck;
+    }
+
+    public void setN_estado_ck(String n_estado_ck) {
+        this.n_estado_ck = n_estado_ck;
+    }
+    
     
     
 }
